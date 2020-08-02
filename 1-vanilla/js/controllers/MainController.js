ee9ce9c6f06467ea845fa464b05448882031f24c
tongnamuu <thban33@naver.com>
2020-08-02 23:42:03 +0900

    Search Form Added

--- 1-vanilla/js/controllers/MainController.js
+++ 1-vanilla/js/controllers/MainController.js
@@ -1,7 +1,10 @@
+import FormView from "../views/FormView";
+import FromView from "../views/FormView.js";
 const tag = "[MainController]";
 
 export default {
   init() {
     console.log(tag, "init()");
+    FromView.setup(document.querySelector("form"));
   },
 };
