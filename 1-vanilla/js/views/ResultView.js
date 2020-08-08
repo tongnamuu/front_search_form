59d7a812df41739efb4b93781d771994d52cc69a
tongnamuu <thban33@naver.com>
2020-08-08 23:38:31 +0900

    search data showing

--- 1-vanilla/js/views/ResultView.js
+++ 1-vanilla/js/views/ResultView.js
@@ -1,25 +1,39 @@
-import View from './View.js'
+import View from "./View.js";
 
-const tag = '[ResultView]'
+const tag = "[ResultView]";
 
-const ResultView = Object.create(View)
+const ResultView = Object.create(View);
 
 ResultView.messages = {
-  NO_RESULT: '검색 결과가 없습니다'
-}
+  NO_RESULT: "검색 결과가 없습니다",
+};
 
 ResultView.setup = function (el) {
-  this.init(el)
-}
+  this.init(el);
+};
 
 ResultView.render = function (data = []) {
-  console.log(tag, 'render()', data)
-  this.el.innerHTML = data.length ? this.getSearchResultsHtml(data) : this.messages.NO_RESULT
-  this.show()
-}
+  console.log(tag, "render()", data);
+  this.el.innerHTML = data.length
+    ? this.getSearchResultsHtml(data)
+    : this.messages.NO_RESULT;
+  this.show();
+};
 
 ResultView.getSearchResultsHtml = function (data) {
-  debugger
-}
+  return (
+    data.reduce((html, item) => {
+      html += this.getSearchItemHtml(item);
+      return html;
+    }, "<ul>") + "</ul>"
+  );
+};
 
-export default ResultView
+ResultView.getSearchItemHtml = function (item) {
+  return `<li>
+        <img src="${item.image}">
+        <p>${item.name}</p>
+    </li>`;
+};
+
+export default ResultView;
