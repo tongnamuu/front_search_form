14e65bb1f86e0ac8143a77de977ef0af0ab77653
tongnamuu <thban33@naver.com>
2020-08-08 23:15:50 +0900

    search result form

--- 1-vanilla/js/controllers/MainController.js
+++ 1-vanilla/js/controllers/MainController.js
@@ -1,19 +1,35 @@
-import FormView from '../views/FormView.js'
+import FormView from "../views/FormView.js";
+import ResultView from "../views/ResultView.js";
 
-const tag = '[MainController]'
+import SearchModel from "../models/SearchModel.js";
+
+const tag = "[MainController]";
 
 export default {
   init() {
-    FormView.setup(document.querySelector('form'))
-      .on('@submit', e => this.onSubmit(e.detail.input))
-      .on('@reset', e => this.onResetForm())
-},
+    FormView.setup(document.querySelector("form"))
+      .on("@submit", (e) => this.onSubmit(e.detail.input))
+      .on("@reset", (e) => this.onResetForm());
+    ResultView.setup(document.querySelector("#search-result"));
+  },
+
+  search(query) {
+    console.log(tag, "search()", query);
+    SearchModel.list(query).then((data) => {
+      this.onSearchResut(data);
+    });
+  },
 
   onSubmit(input) {
-    console.log(tag, 'onSubmit()', input)
+    console.log(tag, "onSubmit()", input);
+    this.search(input);
   },
 
   onResetForm() {
-    console.log(tag, 'onResetForm()')
-  }
-}
+    console.log(tag, "onResetForm()");
+  },
+
+  onSearchResut(data) {
+    ResultView.render(data);
+  },
+};
