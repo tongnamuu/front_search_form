9640212fad657a45e653aa4cf21b8b17a44c492a
tongnamuu <thban33@naver.com>
2020-08-25 02:45:08 +0900

    Result Deleted

--- 1-vanilla/js/controllers/MainController.js
+++ 1-vanilla/js/controllers/MainController.js
@@ -1,35 +1,36 @@
-import FormView from '../views/FormView.js'
-import ResultView from '../views/ResultView.js'
+import FormView from "../views/FormView.js";
+import ResultView from "../views/ResultView.js";
 
-import SearchModel from '../models/SearchModel.js'
+import SearchModel from "../models/SearchModel.js";
 
-const tag = '[MainController]'
+const tag = "[MainController]";
 
 export default {
   init() {
-    FormView.setup(document.querySelector('form'))
-      .on('@submit', e => this.onSubmit(e.detail.input))
-      .on('@reset', e => this.onResetForm())
+    FormView.setup(document.querySelector("form"))
+      .on("@submit", (e) => this.onSubmit(e.detail.input))
+      .on("@reset", (e) => this.onResetForm());
 
-    ResultView.setup(document.querySelector('#search-result'))
+    ResultView.setup(document.querySelector("#search-result"));
   },
 
   onSubmit(input) {
-    console.log(tag, 'onSubmit()', input)
-    this.search(input)
+    console.log(tag, "onSubmit()", input);
+    this.search(input);
   },
 
   onResetForm() {
-    console.log(tag, 'onResetForm()')
+    console.log(tag, "onResetForm()");
+    ResultView.hide();
   },
 
   search(query) {
-    SearchModel.list(query).then(data => {
-      this.onSearchResult(data)
-    })
+    SearchModel.list(query).then((data) => {
+      this.onSearchResult(data);
+    });
   },
 
   onSearchResult(data) {
-    ResultView.render(data)
-  }
-}
+    ResultView.render(data);
+  },
+};
